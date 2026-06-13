17ef6a785cf33983cff7c371edc76076bcee92ee
veecodekub <74323401+veecodekub@users.noreply.github.com>
2026-06-14 01:22:43 +0700

    remove impeccable-live

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}{/* impeccable-live-start */}
-<script src="http://localhost:8400/live.js"></script>
-{/* impeccable-live-end */}
-</body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
